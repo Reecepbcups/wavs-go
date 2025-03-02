8b2b8ea1901373bcf404254b2471822cf2ea0736
Reece Williams <reecepbcups@gmail.com>
2025-03-01 20:23:07 -0600

    Ok, OkayNone, Err aliases

--- types/trigger_result.go
+++ types/trigger_result.go
@@ -8,3 +8,18 @@ import (
 // TriggerResult is the return type for wavs trigger world Run function
 // an alias of wavs Exports Run() result type
 type TriggerResult = cm.Result[wavs.OptionListU8Shape, cm.Option[cm.List[uint8]], string]
+
+// Ok (alias) returns an Ok response of type Some
+func Ok(resp []byte) TriggerResult {
+	return cm.OK[TriggerResult](cm.Some(cm.NewList(&resp[0], len(resp))))
+}
+
+// OkNone (alias) returns an Ok response of type None
+func OkNone() TriggerResult {
+	return cm.OK[TriggerResult](cm.None[cm.List[uint8]]())
+}
+
+// Err (alias) returns an Err response of type string
+func Err(err string) TriggerResult {
+	return cm.Err[TriggerResult](err)
+}
